25f6fe71e69231f14fb27e09857f4de39cfb6c6b
Souvik <souvik97381@gmail.com>
2022-11-13 01:05:25 +0530

    Nov06 codes added

---
+++ src/BookStock.js
@@ -0,0 +1,27 @@
+export const BookStock = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/71A-8Cr9cxL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Blockchain Technology: Concepts and Applications',
+    author: 'Kumar Saurabh and Ashutosh Saxena'
+  }
+  , {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/61ItRb1o7nL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'MASTERING BITCOIN 2/ED PROGRAMMING THE OPEN BLOCKCHAIN',
+    author: 'Andreas M. Antonopoulos '
+  }
+
+  , {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/51i0-FJQZjL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'BLOCKCHAIN',
+    author: 'Don Tapscott'
+  }
+  , {
+    id: 4,
+    img: 'https://m.media-amazon.com/images/I/51i0-FJQZjL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'BLOCKCHAIN',
+    author: 'Don Tapscott'
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BookStock } from './BookStock'; // named import , {} mandatory , name can't be changed
+import Books from './Books'; // default import , {} is strictly not allowed , name can be changed
 
-const Book1 = {
-  image: "https://rukminim1.flixcart.com/image/612/612/khxqt8w0-0/book/9/t/k/java-the-complete-reference-eleventh-edition-original-imafxuybqqhdthsc.jpeg?q=70",
-  name: "Java the Complete Reference"
-}
 
-const Book2 = {
-  image: "https://rukminim1.flixcart.com/image/416/416/kjbr8280-0/book/d/d/l/think-java-how-to-think-like-a-computer-scientist-original-imafyxhmzwmqhfby.jpeg?q=70",
-  name: "Think Java How To Think Like A Computer Scientist - How to Think Like a Computer Scientist"
-}
+
+
+
+
+const names = ["Navin", "Sanjeevan", "Hyder"];
+
+const namesThroughMap = names.map((nm) => {
+  return <h1>{nm}</h1>
+})
+
+// const BookStockThroughMap = BookStock.map((book) => {
+//   const { img, title, author } = book;
+//   return <Books book={book}></Books>
+// })
 
 const Index = () => {
   return (
     <>
-      <div><h1 className="textCenter"><u>Hello React !! ( Hyder version )</u></h1></div>
-      <Student></Student>
-      <Student />
-      <div className='somedecorations'>
-        <Books image={Book1.image} author={Book1.name} />
-        <Books image={Book2.image} author={Book2.name}></Books>
-      </div>
-    </>
-  );
-}
-
-let Student = () => {
-  return (
-    <h3>Yes, I am a student !!</h3>
-  );
-}
-
-let Books = (pp) => {
-  const { image, author } = pp;
-  return (
-    <>
-      <img src={image} alt="Default image is not here" />
-      <h3>{author}</h3>
+      <section className='bookList'>{namesThroughMap}</section>
+      {/* For this no key has been passed, so warning comes , but for the below section there is no warning because I have used key there */}
+      {/* Array can be rendered directly */}
+      <section className='bookList'>{BookStock.map((book) => {
+        const { img, title, author } = book; // what ever the type u mentioned here, #
+        // return <Books key={book.id} book={book}></Books>
+        return <Books key={book.id} {...book}></Books>
+      })}
+      </section>
+      {/* Array of objects, directly can not be rendered directly */}
     </>
   );
 }
